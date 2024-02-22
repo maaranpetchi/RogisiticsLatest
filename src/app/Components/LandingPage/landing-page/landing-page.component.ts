1d5b32e41ebde1ad1e9c86b045ce7bdc8e1b679b
Petchiappan.P <Petchiappan.P@KTP-KIT008>
2024-02-22 18:00:00 +0530

    Scrolling Completed

--- src/app/Components/LandingPage/landing-page/landing-page.component.ts
+++ src/app/Components/LandingPage/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
+
+  constructor(private elRef: ElementRef) {}
+
   isNavbarCollapsed = true;
 
   toggleNavbar() {
     this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  scrollToSection(section: string): void {
+    const element = this.elRef.nativeElement.querySelector(`#${section}`);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
   }
 }
